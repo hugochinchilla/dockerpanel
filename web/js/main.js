3c4f40264b6b467af21805390935f66ef5c9e224
Hugo Chinchilla Carbonell <hchinchilla@habitissimo.com>
2014-11-28 16:10:38 +0100

    trying eve-api for the client

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,16 @@
-angular.module('app', ['components'])
+angular
+  .module('DockerPanel', ['ngResource', 'eveApi'])
 
-.controller('UserController', ['$scope'], UserController)
-.controller('DomainController', ['$scope'], DomainController)
-.config();
+  .constant('config', {
+    server: { url: 'http://127.0.0.1:5000' }
+  })
+  .config(["apiProvider", function(apiProvider) {
+    apiProvider.api('users', { type:'http', backend: { rel:'users' }});
+  }])
+  /*
+  .factory("User", function(api) {
+    //console.log(api);
+  })
+*/
+  .controller('UserController', ['$scope', 'api', UserController])
+  .controller('DomainController', ['$scope', DomainController]);
