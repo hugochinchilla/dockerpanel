cf258acfecd78b3d1adb9e0b0179ec58e132b53d
Hugo Chinchilla Carbonell <hchinchilla@habitissimo.com>
2014-11-28 16:08:35 +0100

    testing eve for the rest services

---
+++ server/settings.py
@@ -0,0 +1,24 @@
+from schemas.users import users
+
+DOMAIN = {
+  'users': users,
+  #'domains': {},
+}
+
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+X_DOMAINS = '*'
+
+# Let's just use the local mongod instance. Edit as needed.
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
